ba2404c4d7945fc90579d8fea803aa4c826fa03a
SANJEET <sanjeetdutt@gmail.com>
2023-06-29 21:40:49 +0530

    Completed root navigation

--- frontend-ts/src/components/RootNavigation.tsx
+++ frontend-ts/src/components/RootNavigation.tsx
@@ -1,12 +1,34 @@
 import styles from "./RootNavigation.module.scss"
+import {NavLink} from "react-router-dom";
 
 function RootNavigation(){
-    return <div className={styles.navigation}>
-        <ul>
-            <li>Home Page</li>
-            <li>Events</li>
-        </ul>
-    </div>
+
+    const activeClassHandler = (state:{isActive:boolean})=>{
+        if(state.isActive)
+            return styles.active
+        return null
+    }
+
+    return <nav className={styles.navigation}>
+        <div className={styles.navigation__brandName}>
+            <h1>Event Management</h1>
+        </div>
+        <div className={styles.navigation__links}>
+            <ul>
+                <li>
+                    <NavLink to="/" className={activeClassHandler}>
+                        Home Page
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/event" className={activeClassHandler}>
+                        Events
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
+
+    </nav>
 }
 
 export default RootNavigation
