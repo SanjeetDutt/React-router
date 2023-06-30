26983044740c6d7fdaf379a7fa6b4fae9879e280
SANJEET <sanjeetdutt@gmail.com>
2023-06-30 09:36:55 +0530

    Completed home navigation

--- frontend-ts/src/pages/HomePage.tsx
+++ frontend-ts/src/pages/HomePage.tsx
@@ -1,6 +1,11 @@
 function HomePage(){
     return(
-        <h1>Home page</h1>
+        <div className="vh--100 bg--primary d--flex justify-content--center align-items--center">
+            <div className="w--50">
+                <h1 className="text--white">Welcome to Event management system</h1>
+                <p className="text--white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi architecto, assumenda at commodi doloremque error eveniet exercitationem hic impedit ipsum labore magnam magni maxime minus necessitatibus numquam ratione recusandae sequi!</p>
+            </div>
+        </div>
     )
 }
 
